4553571f10d660fecf7eeb604401bdbb51f83291
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2025-08-16 22:49:23 +0800

    feat: sweetalerts modals for login page

---
+++ public/scripts/login.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // forget password modal
+  document.querySelector("a[href='#forgot']").addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+    Swal.fire({
+      title: 'Forgot Password',
+      input: 'email',
+      inputLabel: 'Enter your email address',
+      inputPlaceholder: 'you@example.com',
+      confirmButtonText: 'Continue',
+      customClass: {
+        confirmButton: 'btn btn-primary',
+      },
+      buttonsStyling: false,
+      heightAuto: false,
+      allowOutsideClick: true,
+      showCloseButton: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.reload();
+      }
+    });
+  });
+
+  // login animation modal
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    Swal.fire({
+      title: 'Logging you in...',
+      allowOutsideClick: false,
+      heightAuto: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
+    // fake loading
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 2000);
+  });
+});
