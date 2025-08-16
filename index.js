1922cb79b850bc0a504f8272a51899a7a9cc75f8
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2025-08-16 14:44:43 +0800

    feat: add new routes and views for inventory, about, contact, login, and register pages templates

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ app.use('/assets', express.static('assets'));
 app.get('/', (req, res) => {
   res.render('index', { heading: 'Home Page'});
 });
+
+app.get('/inventory', (req, res) => {
+  res.render('inventory', { heading: 'Inventory'});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { heading: 'About Page'});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { heading: 'Contact Page'});
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { heading: 'Login Page'});
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', { heading: 'Register Page'});
+});
